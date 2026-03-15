2a9bf7d4a332f3f533969591671251d6e971dcb8
Nikola <nikolaxxx@protonmail.com>
2026-03-15 18:06:38 +0400

    chore: clean up packages directory, keep only skill tar.gz distributions

--- packages/browse.skill/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import('./dist/index.js').then(m => {
-  // Module runs automatically when imported directly
-}).catch(err => {
-  console.error('Failed to load browse skill:', err.message);
-  process.exit(1);
-});
